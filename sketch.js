18fde1c4fd83762b9bf964490d67d5f6337258d5
sreshtamandadi <Sreshtamandadi@gmail.com>
2020-07-07 18:06:08 +0530

    C27

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var loger;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +18,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    loger = new Log(600,250,170,PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -35,6 +38,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    chain = new Chain(bird.body,loger.body);
 
 }
 
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    loger.display();
+    chain.display();
 }
